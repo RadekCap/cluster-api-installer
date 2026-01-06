687cde1e6022a19c3cd8a08f6144b97436b0ab13
Marek Veber <mveber@redhat.com>
2026-01-06 20:56:40 +0100

    add --image="kindest/node:v1.31.0" to kind create cluster

--- scripts/setup-kind-cluster.sh
+++ scripts/setup-kind-cluster.sh
@@ -13,7 +13,7 @@ set -e
 KIND_CLUSTER_NAME=${KIND_CLUSTER_NAME:-${1:-aso2}}
 
 if ! (kind get clusters 2>/dev/null|grep -q '^'"$KIND_CLUSTER_NAME"'$') ; then
-    kind create cluster --name "$KIND_CLUSTER_NAME"
+    kind create cluster --name "$KIND_CLUSTER_NAME" --image="kindest/node:v1.31.0"
     helm repo add jetstack https://charts.jetstack.io --force-update
     helm repo update
     helm upgrade --install cert-manager jetstack/cert-manager --namespace cert-manager --create-namespace --set crds.enabled=true --wait --timeout 5m
